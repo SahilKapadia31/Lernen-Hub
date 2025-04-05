fce47dd655b2e30c5a13657e1c8dfe713bf6d3b3
SahilKapadia31 <sahilkapadia89@gmail.com>
2025-04-05 11:24:38 +0530

    Add initial component files for Navbar, HeroSection, Features, Testimonials, and Footer

---
+++ v2/components/Navbar/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Navbar = props => {
+    return (
+        <div>Navbar</div>
+    )
+}
+
---
+++ v2/components/index.js
@@ -0,0 +1,5 @@
+export { Navbar } from './Navbar/Navbar'
+export { HeroSection } from './Hero/HeroSection'
+export { Features } from './Features/Features'
+export { Testimonials } from './Testimonials/Testimonials'
+export { Footer } from './Footer/Footer'
